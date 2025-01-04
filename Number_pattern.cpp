15d8afd3e3055dd1bb04f77dab7024db5261d5a8
Chinmayee259 <chinmayeeburte25@gmail.com>
2025-01-04 12:00:54 +0530

    Update Number_pattern.cpp

--- Number_pattern.cpp
+++ Number_pattern.cpp
@@ -1,9 +1,13 @@
+//Trianagle Pattern
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    for(int i = 1 ; i < 5 ; i++)
+    int n;
+    cout << "Enter a number : ";
+    cin >> n ;
+    for(int i = 1 ; i < n+1 ; i++)
     {
         for(int j = 1 ; j < i+1 ; j++)
         {
