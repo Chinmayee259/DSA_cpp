316d7204cd4e7d150fd3feaa8cdb603dea164665
Chinmayee259 <chinmayeeburte25@gmail.com>
2025-01-04 12:13:33 +0530

    Update Number_pattern.cpp

--- Number_pattern.cpp
+++ Number_pattern.cpp
@@ -1,4 +1,3 @@
-//Trianagle Pattern
 #include<iostream>
 using namespace std;
 
@@ -7,6 +6,8 @@ int main()
     int n;
     cout << "Enter a number : ";
     cin >> n ;
+
+    //Trianagle Pattern
     for(int i = 1 ; i < n+1 ; i++)
     {
         for(int j = 1 ; j < i+1 ; j++)
@@ -15,13 +16,32 @@ int main()
         }
         cout << endl;
     }
+
+        /*
+    n = 4
+    1
+    12
+    123
+    1234
+    *\
+
+   //Reverse triangle
+    for(int i = 1 ; i < n+1 ; i++)
+    {
+        for(int j = i ; j >= 1 ; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    /*
+    1
+    21
+    321
+    4321
+    */
+    
     
     return 0 ;
 }
-/*
-n = 4
-1
-12
-123
-1234
-*\
+
